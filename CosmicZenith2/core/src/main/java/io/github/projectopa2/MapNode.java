282b889c5ad3a685e83f3f85ace301bacd960cfd
FC00000 <filipeac00@gmail.com>
2025-06-13 19:34:00 +0100

    Teste (Espaços)

--- CosmicZenith2/core/src/main/java/io/github/projectopa2/MapNode.java
+++ CosmicZenith2/core/src/main/java/io/github/projectopa2/MapNode.java
@@ -2,9 +2,9 @@ package io.github.projectopa2;
 import com.badlogic.gdx.utils.Array;
 
 public class MapNode {
-    public int x, y;                // As cordenadas do mapa de nodes
-    public boolean isWall;          // Booleano para validar se é uma parede
-    public Array<MapNode> neighbors;// Lista das nodes vizinhas
+    public int x, y;                 // As cordenadas do mapa de nodes
+    public boolean isWall;           // Booleano para validar se é uma parede
+    public Array<MapNode> neighbors; // Lista das nodes vizinhas
 
     public MapNode(int x, int y, boolean isWall) {
         this.x = x;
